e5868d2e5750700a33e55c3b35dbb5e086c65ad7
zhengjunhui <zhengoba@foxmail.com>
2017-03-13 09:37:28 +0800

    应用进程检测脚本

--- promonitor.sh
+++ promonitor.sh
@@ -19,16 +19,16 @@ exit 1
 fi
 }
 
+#应用一
 
 
-
-function tomcat(){
+function sobet(){
      PROT=8080
      HOST_IP=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-     APPNAME=tomcat
-     APPDIR=/root/tomcat
-     Checkproess=`ps -ef|grep tomcat|grep -v grep|wc -l`
-     CheckStatus=`curl -I -m 10 -o /dev/null -s -w %{http_code}"\n"  $HOST_IP:$PROT/admin`
+     APPNAME=sobet
+     APPDIR=/root/sobet
+     Checkproess=`ps -ef|grep $APPNAME|grep -v grep|wc -l`
+     CheckStatus=`curl -I -m 10 -o /dev/null -s -w %{http_code}"\n"  $HOST_IP:$PROT/$APPNAME`
     if [ $Checkproess == 0 ]
     then
 	warn_echo "不好！$APPNAME应用挂掉了，不过不用担心我会帮你启动起来 "
@@ -37,11 +37,28 @@ function tomcat(){
 	info_echo "=========================正在启动${APPNAME}应用=====================" 	
 	sleep 10
 	info_echo "正在检测进程启动状态"
-		if [ $Checkproess -lt 400 ]
+		if [ "$CheckStatus" == "000" ]
+		then
+			warn_echo "$APPNAME应用还未启动完成 HTTP状态码为：$CheckStatus！请等待.........."
+			while true
+			do
+			CheckStatus=`curl -I -m 10 -o /dev/null -s -w %{http_code}"\n"  $HOST_IP:$PROT/$APPNAME`  #重新获取$CheckStatus的值，不然会造成死循环
+				case $CheckStatus in 
+					000) info_echo "正在启动中"
+					;;
+					*) info_echo "启动完成"
+					break
+					;;
+				esac
+			done
+		
+				
+		elif [ $CheckStatus -lt 400 ]
 		then
 			info_echo "$APPNAME应用启动状态正常"
+
 		else
-			err_echo "$APPNAME应用进程启动失败！请手动排查错误并启动"
+			warn_echo "$APPNAME应用进程启动失败 HTTP状态码为：$CheckStatus！请手动排查错误并启动"
 #			echo "主机:$HOST_IP TK环境$APPNAME应用启动失败 " | mail -s "脚本启动$APPNAME失败，请手动启动" shine@networkws.com
 		fi
     else
@@ -49,4 +66,50 @@ function tomcat(){
     fi	
 }
 
-tomcat
+#应用二
+
+function admin(){
+     PROT=8081
+     HOST_IP=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+     APPNAME=admin
+     APPDIR=/root/admin
+     Checkproess=`ps -ef|grep $APPNAME|grep -v grep|wc -l`
+     CheckStatus=`curl -I -m 10 -o /dev/null -s -w %{http_code}"\n"  $HOST_IP:$PROT/$APPNAME`
+    if [ $Checkproess == 0 ]
+    then
+	warn_echo "不好！$APPNAME应用挂掉了，不过不用担心我会帮你启动起来 "
+	source /etc/profile
+	/bin/sh 		$APPDIR/bin/startup.sh  >> /dev/null
+	info_echo "=========================正在启动${APPNAME}应用=====================" 	
+	sleep 10
+	info_echo "正在检测进程启动状态"
+		if [ "$CheckStatus" == "000" ]
+		then
+			warn_echo "$APPNAME应用还未启动完成 HTTP状态码为：$CheckStatus！请等待.........."
+			while true
+			do
+			CheckStatus=`curl -I -m 10 -o /dev/null -s -w %{http_code}"\n"  $HOST_IP:$PROT/$APPNAME`  #重新获取$CheckStatus的值，不然会造成死循环
+				case $CheckStatus in 
+					000) info_echo "正在启动中"
+					;;
+					*) info_echo "启动完成"
+					break
+					;;
+				esac
+			done
+		
+				
+		elif [ $CheckStatus -lt 400 ]
+		then
+			info_echo "$APPNAME应用启动状态正常"
+
+		else
+			warn_echo "$APPNAME应用进程启动失败 HTTP状态码为：$CheckStatus！请手动排查错误并启动"
+#			echo "主机:$HOST_IP TK环境$APPNAME应用启动失败 " | mail -s "脚本启动$APPNAME失败，请手动启动" shine@networkws.com
+		fi
+    else
+	info_echo "$APPNAME应用正在稳定运行当中"
+    fi	
+}
+sobet
+admin
